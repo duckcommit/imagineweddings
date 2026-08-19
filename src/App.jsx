6b6adf651285be775848d1f965b6a8ae42698253
daemontrive <daemontrive@gmail.com>
2026-08-19 20:08:29 +0530

    Update App.jsx
    Updated Phone number

--- src/App.jsx
+++ src/App.jsx
@@ -22,7 +22,7 @@ import Maintenance from './components/Maintenance'
 // Flip to false to bring the site back live
 const MAINTENANCE_MODE = false
 
-const WA_NUMBER = '919325204096'
+const WA_NUMBER = '919112756626'
 const WA_MSG = encodeURIComponent('Hi Imagine Weddings, I want to plan my wedding.')
 
 function WhatsAppButton() {
